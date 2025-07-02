61906abe53a0fb8fb00755e64783fca0a0f061dc
hariomshahu <168951707+hariomshahu@users.noreply.github.com>
2025-07-02 20:39:25 +0000

    Update links to my own repository

--- landing.py
+++ landing.py
@@ -11,7 +11,7 @@
 # contains materials for an 8-week workshop series that will introduce you to the
 # fascinating world of 3D computer vision. Whether you're a beginner or have some
 # programming experience, this course will help you understand and implement key
-# concepts in 3D vision.
+# concepts in 3D vision..
 #
 # ## Deliverables
 #
